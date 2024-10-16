78b87053abc89f7adcaca8abc7f3879cc18b6ba8
Benjamin <benjamin.dumontgirard@gmail.com>
2024-10-16 16:28:03 +0200

    ajout de 2-3 truc

--- src/pub/autres_pages/creation_offre.php
+++ src/pub/autres_pages/creation_offre.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <script src="../script_js/creation_offre.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/style.css">
@@ -8,54 +9,70 @@
 </head>
 <body>
     <?php include("header.php") ?>
-    <section id="titre_creation_offre" >
-<?php 
-    if (isset($_POST)) { ?>
-            <?php 
-            /*
-                afin d'afficher le tire correctement j'effectu un switch sur le type d'offre
-                je ne fais pas <h1>Créer <?php echo ucfirst(str_replace('_',' ',$_POST['type_offre'])) ; \⸮><h1>
-                car il faut gérer le préfixe "un/une" qui nécéssite de toute facons un switch
-            */
-            $type_offre_affichable = '';
-            switch ($_POST["type_offre"]) {
-                case 'spectacle':
-                    $type_offre_affichable = "un spectacle";
-                break;
-                case 'parc_attraction':
-                    $type_offre_affichable = "un parc d'attraction";
-                break;
-                case 'visite':
-                    $type_offre_affichable = "une visite";
-                break;
-                case 'restauration':
-                    $type_offre_affichable = "une restauration";
-                break;
-                case 'activite':
-                    $type_offre_affichable = "une activité";
-                break;
-                default:
-                    $type_offre_affichable = "une offre";
-                break;
-                } ?>
-        <h1>Créer <?php echo $type_offre_affichable; ?></h1>
-    <?php
-    }else{ ?> <!-- post is unset-->
-        <h1>Erreur de methode d'acces</h1>
-        <p>Une erreur dans la requette de la page est survenu merci de réessayer plus tard</p>
-    <?php } ?>
-    </section>
-    <section id="image_creation_offre" style="background-color: gray;">
-        
-    </section>
-    <section name="tarif">
+    <main>
+        <section id="titre_creation_offre" >
+        <?php 
+        if (isset($_POST)) { ?>
+                <?php 
+                /*
+                    afin d'afficher le tire correctement j'effectu un switch sur le type d'offre
+                    je ne fais pas <h1>Créer <?php echo ucfirst(str_replace('_',' ',$_POST['type_offre'])) ; \⸮><h1>
+                    car il faut gérer le préfixe "un/une" qui nécéssite de toute facons un switch
+                */
+                $type_offre_affichable = '';
+                switch ($_POST["type_offre"]) {
+                    case 'spectacle':
+                        $type_offre_affichable = "un spectacle";
+                    break;
+                    case 'parc_attraction':
+                        $type_offre_affichable = "un parc d'attraction";
+                    break;
+                    case 'visite':
+                        $type_offre_affichable = "une visite";
+                    break;
+                    case 'restauration':
+                        $type_offre_affichable = "une restauration";
+                    break;
+                    case 'activite':
+                        $type_offre_affichable = "une activité";
+                    break;
+                    default:
+                        $type_offre_affichable = "une offre";
+                    break;
+                    } ?>
+            <h1>Créer <?php echo $type_offre_affichable; ?></h1>
+        <?php
+        }else{ ?> <!-- post is unset-->
+            <h1>Erreur de methode d'acces</h1>
+            <p>Une erreur dans la requette de la page est survenu merci de réessayer plus tard</p>
+        <?php } ?>
+        </section>
+        <section id="image_creation_offre" style="background-color: gray;">
+            <p style="background-color: gray;">À ajouter plus tard</p>
+        </section>
+        <form action="" method="post">
 
-    </section>
-</body>
+            <section name="tarif">
+                <article>
+                    <h3>Tarifs</h3>
+                    <!-- ajouter la taille dans le css -->
+                    <button onclick="ajoutTarif();"><img src="../images/icon/icons8-ajouter-90.png" alt="Ajouter tarif"></button>
+                </article>
+                <article id="tarif_ajoute">
+                    
+                </article>
+                <section>
+
+                </section>
+            </section>
+            <button type="submit">Créer <?php echo $type_offre_affichable; ?></button>
+        </form>
+    </main>
     <footer>
         <?php
             //include("footer.html"); TODO
-        ?>
+            ?>
     </footer>
+</body>
 
 </html>
---
+++ src/pub/script_js/creation_offre.js
@@ -0,0 +1,5 @@
+let id_ajout_tarif = 0;
+function ajoutTarif() {
+    id_ajout_tarif++;
+    //document.getElementById('tarif_ajoute').innerHTML+='<br/><input type="text" name="nom_tarif_'+id_ajout_tarif+'" placeholder="Nom du tarif n°'+id_ajout_tarif+'"  /><br/><input type="number" name="prix_tarif_'+id_ajout_tarif+'" placeholder="Prix du tarif n°'+id_ajout_tarif+'"  /><br/>';
+}
