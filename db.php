94f3e731e360a4356fbf9de62377bacc7af4eb20
Scover <thediscover22450@gmail.com>
2024-10-17 09:34:13 +0200

    added password

--- db.php
+++ db.php
@@ -7,6 +7,7 @@ const DB_USER   = 'sae';
 function db_connect(): PDO {
     return new PDO(
         DB_DRIVER.':host='.DB_SERVER.';dbname='.DB_NAME,
-        DB_USER
+        DB_USER,
+        getenv('DB_ROOT_PASSWORD')
     );
 }
