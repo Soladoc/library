2900ef764f8677f83734509b6e6c2bb24b81512c
Scover <thediscover22450@gmail.com>
2024-10-17 11:12:13 +0200

    header: lien vers accueil

--- html/autres_pages/header.php
+++ html/autres_pages/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="logo">
-        <img src="../images/logo.png" alt="Logo pact">
+        <a href="/"><img src="../images/logo.png" alt="Logo pact"></a>
     </div>
     <div class="auth-button">
         <img src="../images/profile-icon.png" alt="Profil">
