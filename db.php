80c70b136eeccee7c6f679a782a75ac4c09db384
Scover <thediscover22450@gmail.com>
2024-10-17 11:03:48 +0200

    dotenv

--- db.php
+++ db.php
@@ -16,8 +16,30 @@ function notfalse(mixed $value, string $msg): mixed
     return $value;
 }
 
+$_dotenv_loaded = false;
+
+function load_dotenv()
+{
+    global $_dotenv_loaded;
+    
+    if ($_dotenv_loaded) {
+        return;
+    }
+
+    $env = notfalse(file_get_contents(__DIR__ . '/.env'), 'dotenv file missing');
+    foreach (explode("\n", $env) as $line) {
+        preg_match('/([^#]+)\=(.*)/', $line, $matches);
+        if (isset($matches[2])) {
+            putenv(trim($line));
+        }
+    }
+
+    $_dotenv_loaded = true;
+}
+
 function db_connect(): PDO
 {
+    load_dotenv();
     return new PDO(
         'psgsql:host=postgresdb;port=' . notfalse(getenv('PGDB_PORT'), 'PGDB_PORT not set') . ';dbname=postgres',
         notfalse(getenv('DB_USER'), 'DB_USER not set'),
