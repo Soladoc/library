f791143654c59f62836eac9309c7135988e73630
Scover <thediscover22450@gmail.com>
2024-10-17 10:43:54 +0200

    connect

--- db.php
+++ db.php
@@ -1,13 +1,8 @@
 <?php
-const DB_SERVER = 'postgresdb';
-const DB_DRIVER = 'pgsql';
-const DB_NAME = 'postgres';
-const DB_USER   = 'sae';
-
 function db_connect(): PDO {
     return new PDO(
-        DB_DRIVER.':host='.DB_SERVER.';dbname='.DB_NAME,
-        DB_USER,
+        'psgsql:host=postgresdb;port='.getenv('PGDB_PORT').';dbname=postgres',
+        getenv('DB_USER'),
         getenv('DB_ROOT_PASSWORD')
     );
 }
