d055d82fb68e34c44324d34434486aade76c2ef0
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-10-21 14:07:28 +0200

    remise vuebdd

--- sql/vuesBDD.sql
+++ sql/vuesBDD.sql
@@ -1,5 +1,5 @@
 set schema 'pact';
-create or replace view "membres" as select * from _membre natural join _compte where existe= true;
+create or replace view membres as select * from _membre natural join _compte where existe= true;
 create or replace view offres as select * from _offre;
 create or replace view pro_prive as select * from _professionnel natural join _prive natural join _compte;
 create or replace view pro_public as select * from _professionnel join _public on id_professionnel=id_public natural join _compte;
