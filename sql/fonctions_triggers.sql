4d8d2503005a28c8887b52dda825b83f95a98305
Soladoc <romaingrandchamppro@gmail.com>
2024-10-21 08:54:42 +0200

    fonctions et triggers complets (inserts)

--- sql/fonctions_triggers.sql
+++ sql/fonctions_triggers.sql
@@ -1,9 +1,10 @@
 set schema 'pact';
 create or replace function membres_insert() returns trigger as $$
-declare 
+declare
   id_temp integer;
 begin
-  insert into _identite() values () returning id_identite into id_temp;
+  select max(id_identite) as id_tempo from pact._identite;
+  insert into _identite(id_identite) values (id_tempo+1) returning id_identite into id_temp;
   insert into _compte(email,mdp_hash,nom,prenom,telephone,id_identite) values (new.email,new.mdp_hash,new.nom,new.prenom,new.telephone,id_temp);
   insert into _membre(pseudo,existe,email) values (new.pseudo,true,new.email);
   return new;
@@ -17,7 +18,8 @@ create or replace function pro_prive_insert() returns trigger as $$
 declare 
   id_temp integer;
 begin
-  insert into _identite() values () returning id_identite into id_temp;
+  select max(id_identite) as id_tempo from pact._identite;
+  insert into _identite(id_identite) values (id_tempo+1) returning id_identite into id_temp;
   insert into _compte(email,mdp_hash,nom,prenom,telephone,id_identite) values (new.email,new.mdp_hash,new.nom,new.prenom,new.telephone,id_temp);
   insert into _professionnel(denomination,email) values (new.denomination,new.email) returning id_professionnel into id_temp;
   insert into _prive(id_prive,siren) values (id_temp,new.siren);
@@ -32,7 +34,8 @@ create or replace function pro_public_insert() returns trigger as $$
 declare 
   id_temp integer;
 begin
-  insert into _identite() values () returning id_identite into id_temp;
+  select max(id_identite) as id_tempo from pact._identite;
+  insert into _identite(id_identite) values (id_tempo+1) returning id_identite into id_temp;
   insert into _compte(email,mdp_hash,nom,prenom,telephone,id_identite) values (new.email,new.mdp_hash,new.nom,new.prenom,new.telephone,id_temp);
   insert into _professionnel(denomination,email) values (new.denomination,new.email) returning id_professionnel into id_temp;
   insert into _public(id_public) values (id_temp);
