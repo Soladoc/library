4ae636360a2a7fa643065fddb200144f1ac5f4dc
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-10-21 15:51:13 +0200

    essai favorable 14

--- html/connexion/login.php
+++ html/connexion/login.php
@@ -22,14 +22,14 @@ if (empty($username) || empty($password)) {
 }
 
 // Préparer et exécuter la requête pour éviter les injections SQL
-$stmt = $pdo->prepare("SELECT email, mdp_hash FROM pact._compte WHERE email = :email");
+$stmt = $pdo->prepare("SELECT c.email, c.mdp_hash, m.existe FROM pact._compte AS c JOIN pact._membre AS m ON c.email = m.email WHERE c.email = :email AND m.existe = true");
 $stmt->bindValue(':email', $username, PDO::PARAM_STR);
 
 $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 // Vérifier si l'utilisateur existe
-if ($user) {
+if ($user['existe'] == true) {
     
     $hashed_password = $user['mdp_hash'];
     echo "Hash récupéré : " . $hashed_password;
