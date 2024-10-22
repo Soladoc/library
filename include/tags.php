55a05cf4fd58a7dffad7f88b0bfa9fef7dbd3472
Scover <thediscover22450@gmail.com>
2024-10-22 14:43:28 +0200

    fixed distant docker-compose

---
+++ include/tags.php
@@ -0,0 +1,7 @@
+<?php const DEFAULT_TAGS = [
+    'culturel', 'patrimoine', 'histoire', 'urbain', 'nature', 'musée', 'atelier', 'musique', 'famille', 'cinéma', 'cirque', 'son et lumière', 'humour'
+];
+
+const TAGS_RESTAURATION = [
+    'française', 'fruits de mer', 'asiatique', 'indienne', 'gastronomique', 'restauration rapide', 'crêperie'
+];
