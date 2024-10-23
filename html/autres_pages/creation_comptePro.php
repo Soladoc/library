92f2bcba805c3bf479b2f9b7b5e08d2e3c6ecf80
Benjamin <benjamin.dumontgirard@gmail.com>
2024-10-23 15:02:59 +0200

    je rentre

--- html/autres_pages/creation_comptePro.php
+++ html/autres_pages/creation_comptePro.php
@@ -2,6 +2,7 @@
 
 require_once 'db.php';
 if (isset($_POST['mdp'])) {
+    echo "ici on est bien";
     print 'Votre nom :' . $_POST['nom'];
     print 'Votre prenom :' . $_POST['prenom'];
     print 'Votre numero de telephone :' . $_POST['telephone'];
@@ -24,6 +25,7 @@ if (isset($_POST['mdp'])) {
 
 
     if ($estprive) {
+        echo 'oeoeeo';
         // insert in pro_prive
         $sql = 'INSERT INTO  pact.pro_prive (email, mdp_hash, nom, prenom, telephone, denomination, siren) VALUES (:email, :mdp_hash, :nom, :prenom, :telephone, :denomination, :siren)';
         $stmt = $pdo->prepare($sql);
@@ -47,7 +49,6 @@ if (isset($_POST['mdp'])) {
             ':siren' => $_POST['siren']
         ]);
 
-        echo "caca";
         echo "<script>window.location.href='../autres_pages/connexion.php';</script>";
     } else {
         // insert in pro_public
