270afd3d5d6fef892aa0e889281a479e10b5b922
Scover <thediscover22450@gmail.com>
2024-10-24 00:33:52 +0200

    fmt

--- sql/vuesBDD.sql
+++ sql/vuesBDD.sql
@@ -1,7 +1,48 @@
 set schema 'pact';
-create or replace view membres as select * from _membre natural join _compte where existe= true;
-create or replace view offres as select * from _offre;
-create or replace view pro_prive as select * from _professionnel join _prive on id_professionnel=id_prive natural join _compte;
-create or replace view pro_public as select * from _professionnel join _public on id_professionnel=id_public natural join _compte;
-create or replace view avis as select * from _avis;
-create or replace view tous_comptes_pro as select email,mdp_hash,existe,id_identite from _compte natural join _professionnel;
+
+create or replace view membres as
+select
+    *
+from
+    _membre
+    natural join _compte
+where
+    existe = true;
+
+create or replace view offres as
+select
+    *
+from
+    _offre;
+
+create or replace view pro_prive as
+select
+    *
+from
+    _professionnel
+    join _prive on id_professionnel = id_prive
+    natural join _compte;
+
+create or replace view pro_public as
+select
+    *
+from
+    _professionnel
+    join _public on id_professionnel = id_public
+    natural join _compte;
+
+create or replace view avis as
+select
+    *
+from
+    _avis;
+
+create or replace view tous_comptes_pro as
+select
+    email,
+    mdp_hash,
+    existe,
+    id_identite
+from
+    _compte
+    natural join _professionnel;
