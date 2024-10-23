a933ebf4d4a7d9844a0dec90f6b514d267821733
5cover <thediscover22450@gmail.com>
2024-10-23 10:26:48 +0200

    frontend bddd

--- html/autres_pages/creation_offre.php
+++ html/autres_pages/creation_offre.php
@@ -23,7 +23,7 @@ const TYPE_OFFRE_AFFICHABLE = [
 
 <body>
     <?php require 'component/header.php' ?>
-    <template id="template-tarif-row">
+    <template id="template-tarif-tr">
         <tr>
             <td></td>
             <td><input name="input-montant-tarif" type="number" min="0" placeholder="Prix" required> €</td>
@@ -31,106 +31,27 @@ const TYPE_OFFRE_AFFICHABLE = [
         </tr>
     </template>
     <?php if (!isset($_GET) || !($type_offre = $_GET['type-offre'])) { ?>
-    <!-- post is unset-->
-    <h1>Erreur de méthode d'accès</h1>
-    <p>Une erreur dans la requette de la page est survenue, merci de réessayer plus tard</p>
+        <!-- post is unset-->
+        <h1>Erreur de méthode d'accès</h1>
+        <p>Une erreur dans la requette de la page est survenue, merci de réessayer plus tard</p>
 
     <?php exit;
-} ?>
+    } ?>
     <main>
         <section id="titre-creation-offre">
             <h1>Créer <?= TYPE_OFFRE_AFFICHABLE[$type_offre] ?></h1>
         </section>
-        <section id="image-creation-offre" style="background-color: gray;">
-            <h2>Image</h2>
-            <p style="background-color: gray;">À ajouter plus tard</p>
-            <!-- Choisit 1 photo principale + 0 ou plus photos dans la gallerie -->
-        </section>
-        <section id="tarif">
-            <h2>Tarifs</h2>
-            <table id="table-tarifs">
-                <thead>
-                    <tr>
-                        <th>Nom</th>
-                        <th>Montant</th>
-                    </tr>
-                </thead>
-                <tbody id="tbody-tarifs">
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td><input id="nom-tarif" type="text" placeholder="Enfant, Sénior&hellip;"></td>
-                        <td><input id="montant-tarif" type="number" min="0" placeholder="Prix"> €</td>
-                        <td><button id="button-add-tarif" type="button">+</button></td>
-                    </tr>
-                </tfoot>
-            </table>
-        </section>
-        <section id="horaires">
-            <h2>Horaires</h2>
+        <section id="info-generales">
+            <h2>Informations générales</h2>
             <div>
-                <article id="lundi">
-                    <div>
-                        <h3>lundi</h3>
-                        <button type="button" onclick="ajoutHorraire('lundi')">
-                            <img src="../icon/icons8-ajouter-90.png" alt="Ajouter Horraire">
-                        </button>
-                    </div>
-                    <!-- ici se trouverons les horaires ajoutés en javaScript -->
-                </article>
-                <article id="mardi">
-                    <div>
-                        <h3>mardi</h3>
-                        <button type="button" onclick="ajoutHorraire('mardi')">
-                            <img src="../icon/icons8-ajouter-90.png" alt="Ajouter Horraire">
-                        </button>
-                    </div>
-                </article>
-                <article id="mercredi">
-                    <div>
-                        <h3>mercredi</h3>
-                        <button type="button" onclick="ajoutHorraire('mercredi')">
-                            <img src="../icon/icons8-ajouter-90.png" alt="Ajouter Horraire">
-                        </button>
-                    </div>
-                </article>
-                <article id="jeudi">
-                    <div>
-                        <h3>Jeudi</h3>
-                        <button type="button" onclick="ajoutHorraire('jeudi')">
-                            <img src="../icon/icons8-ajouter-90.png" alt="Ajouter Horraire">
-                        </button>
-                    </div>
-                </article>
-                <article id="vendredi">
-                    <div>
-                        <h3>Vendredi</h3>
-                        <button type="button" onclick="ajoutHorraire('vendredi')">
-                            <img src="../icon/icons8-ajouter-90.png" alt="Ajouter Horraire">
-                        </button>
-                    </div>
-                </article>
-                <article id="samedi">
-                    <div>
-                        <h3>Samedi</h3>
-                        <button type="button" onclick="ajoutHorraire('samedi')">
-                            <img src="../icon/icons8-ajouter-90.png" alt="Ajouter Horraire">
-                        </button>
-                    </div>
-                </article>
-                <article id="dimanche">
-                    <div>
-                        <h3>Dimanche</h3>
-                        <button type="button" onclick="ajoutHorraire('dimanche')">
-                            <img src="../icon/icons8-ajouter-90.png" alt="Ajouter Horraire">
-                        </button>
-                    </div>
-                </article>
-            </div>
-        </section>
-        <section id="info-localisation">
-            <h2>Localisation</h2>
-            <div>
+                <label for="titre">Titre*</label>
+                <p>
+                    <input type="text" name="titre" id="titre" maxlength="255" required>
+                </p>
+                <label for="resume">Resumé*</label>
+                <p>
+                    <input type="text" name="resume" id="resume" maxlength="1023" required>
+                </p>
                 <label for="adresse">Adresse*</label>
                 <p>
                     <input type="text" name="adresse" id="adresse" readonly required>
@@ -146,20 +67,82 @@ const TYPE_OFFRE_AFFICHABLE = [
                 </p>
             </div>
         </section>
+        <section>
+            <h2>Photo principale</h2>
+            <input type="file" id="photo-principale" name="photo-principale" accept="image/*" required>
+            <div id="photo-principale-preview"></div>
+        </section>
+        <section id="tarif">
+            <h2>Tarifs</h2>
+            <table id="table-tarifs">
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Montant</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td><input id="nom-tarif" type="text" placeholder="Enfant, Sénior&hellip;"></td>
+                        <td><input id="montant-tarif" type="number" min="0" placeholder="Prix"> €</td>
+                        <td><button id="button-add-tarif" type="button">+</button></td>
+                    </tr>
+                </tfoot>
+            </table>
+        </section>
+        <section id="horaires">
+            <h2>Horaires</h2>
+            <div>
+                <?php
+                function article_horaires(string $jour) { ?>
+                    <template id="template-horaire-tr-<?= $jour ?>">
+                        <tr>
+                            <td><input type="time" name="heure-debut-<?= $jour ?>"></td>
+                            <td><input type="time" name="heure-fin-<?= $jour ?>"></td>
+                            <td><button type="button">-</button></td>
+                        </tr>
+                    </template>
+                    <article id="<?= $jour ?>">
+                        <h3><?= ucfirst($jour) ?></h3>
+                        <button id="button-add-horaire-<?= $jour ?>" type="button">+</button>
+                        <table id="table-horaires-<?= $jour ?>">
+                            <thead>
+                                <tr>
+                                    <th>Début</th>
+                                    <th>Fin</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </article>
+                <?php }
+                foreach (['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'] as $jour) {
+                    article_horaires($jour);
+                }
+                ?>
+            </div>
+        </section>
         <section id="tags">
             <h2>Tags</h2>
             <ul id="list-tag">
                 <?php
-                    require_once 'tags.php';
-                    foreach ($type_offre === 'restauration' ? TAGS_RESTAURATION : DEFAULT_TAGS as $id => $name) {
-                ?><li><label for="tag-<?= $id ?>"><?= $name ?><input type="checkbox" name="tag-<?= $id ?>" id="tag-<?= $id ?>" vaelu></li></label><?php
-                    }
-                    ?>
+                require_once 'tags.php';
+                foreach ($type_offre === 'restauration' ? TAGS_RESTAURATION : DEFAULT_TAGS as $id => $name) { ?>
+                    <li><label for="tag-<?= $id ?>"><?= $name ?><input type="checkbox" name="tag-<?= $id ?>" id="tag-<?= $id ?>" vaelu></li></label>
+                <?php } ?>
             </ul>
         </section>
         <section id="description">
             <h2>Description</h2>
             <textarea name="description" id="description"></textarea>
+        </section>
+        <section id="image-creation-offre">
+            <h2>Gallerie</h2>
+            <input type="file" id="gallerie" name="gallerie" accept="image/*" multiple>
+            <ul id="gallerie-preview"></ul>
         </section>
         <form id="form-offre" action="" method="post">
             <button type="submit">Valider</button>
--- html/script_js/creation_offre.js
+++ html/script_js/creation_offre.js
@@ -1,51 +1,87 @@
+// @ts-nocheck
 "use strict";
 
-/** @type {HTMLTableSectionElement} */
-const tbody_tarifs = document.getElementById('tbody-tarifs');
-
+const tbody_tarifs = document.getElementById('table-tarifs').querySelector('tbody');
 /** @type {HTMLTemplateElement} */
-const template_tarif_row = document.getElementById('template-tarif-row');
-
+const template_tarif_tr = document.getElementById('template-tarif-tr');
 /** @type {HTMLButtonElement} */
 const button_add_tarif = document.getElementById('button-add-tarif');
-
 /** @type {HTMLInputElement} */
 const nom_tarif = document.getElementById('nom-tarif');
-
 /** @type {HTMLInputElement} */
 const montant_tarif = document.getElementById('montant-tarif');
+/** @type {HTMLInputElement} */
+const photo_principale = document.getElementById('photo-principale');
+const photo_principale_preview = document.getElementById('photo-principale-preview');
+const gallerie = document.getElementById('gallerie');
+const gallerie_preview = document.getElementById('gallerie-preview');
 
 const tr_tarif_id_prefix = 'tr-tarif-';
 
-add_tarif_row('Adulte', 10);
+add_tarif_tr('Adulte', 10);
 
 nom_tarif.addEventListener('input', update_can_add_tarif);
 montant_tarif.addEventListener('input', update_can_add_tarif);
 
-button_add_tarif.addEventListener('click', () => add_tarif_row(nom_tarif.value, montant_tarif.valueAsNumber));
+button_add_tarif.addEventListener('click', () => add_tarif_tr(nom_tarif.value, montant_tarif.valueAsNumber));
+
+photo_principale.addEventListener('change', () => preview_image(photo_principale, photo_principale_preview));
+
+gallerie.addEventListener('change', () => preview_image(gallerie, gallerie_preview));
+
+for (const jour of ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']) {
+    /** @type {HTMLButtonElement} */
+    const button_add_horaire = document.getElementById('button-add-horaire-' + jour);
+    const tbody_horaires = document.getElementById('table-horaires-' + jour).querySelector('tbody');
+    button_add_horaire.addEventListener('click', () => tbody_horaires.appendChild(create_horaire_tr(jour)));
+    button_add_horaire.click();
+}
+
+/**
+ * 
+ * @param {string} jour 
+ * 
+ * @return {HTMLTableRowElement}
+ */
+function create_horaire_tr(jour) {
+    /** @type {HTMLTableRowElement} */
+    const tr_horaire = document.getElementById('template-horaire-tr-' + jour).content.children[0].cloneNode(true);
+    /** @type {HTMLInputElement} */
+    const debut = tr_horaire.children[0];
+    /** @type {HTMLInputElement} */
+    const fin = tr_horaire.children[1];
+    /** @type {HTMLButtonElement} */
+    const btn_remove = tr_horaire.children[2];
+
+    debut.addEventListener('input', () => fin.min = debut.value);
+
+    btn_remove.addEventListener('click', () => tr_horaire.remove());
+
+    return tr_horaire;
+}
 
 /**
  * @param {string} nom 
  * @param {number} montant
  */
-function add_tarif_row(nom, montant) {
+function add_tarif_tr(nom, montant) {
     /** @type {HTMLTableRowElement} */
-    const tarif_row = template_tarif_row.content.children[0].cloneNode(true); // Clone the template
+    const tr_tarif = template_tarif_tr.content.children[0].cloneNode(true); // Clone the template
 
-    tarif_row.id = tr_tarif_id_prefix + nom;
-    tarif_row.children[0].innerText = nom;
+    tr_tarif.id = tr_tarif_id_prefix + nom;
+    tr_tarif.children[0].innerText = nom;
 
-    tarif_row.children[1].children[0].value = montant;
+    tr_tarif.children[1].children[0].value = montant;
 
     /** @type {HTMLButtonElement} */
-    const remove_button = tarif_row.children[2];
+    const remove_button = tr_tarif.children[2];
     remove_button.addEventListener('click', () => {
-        tarif_row.remove();
+        tr_tarif.remove();
         update_can_add_tarif();
         update_can_remove_tarif();
     });
 
-    tbody_tarifs.appendChild(tarif_row);
+    tbody_tarifs.appendChild(tr_tarif);
     update_can_add_tarif();
     update_can_remove_tarif();
 }
@@ -61,4 +97,36 @@ function update_can_add_tarif() {
     button_add_tarif.disabled = !nom_tarif.value
         || isNaN(montant_tarif.valueAsNumber)
         || document.getElementById(tr_tarif_id_prefix + nom_tarif.value) !== null;
+}
+
+/**
+ * 
+ * @param {HTMLInputElement} input 
+ * @param {HTMLElement} image_preview_container 
+ */
+function preview_image(input, image_preview_container) {
+    image_preview_container.textContent = '';
+
+    for (const file of input.files) {
+        if (!file.type.match('image.*')) {
+            continue;
+        }
+
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function (event) {
+            const imageUrl = event.target.result;
+            const image = new Image();
+
+            image.addEventListener('load', function () {
+                image_preview_container.appendChild(image);
+            });
+
+            image.src = imageUrl;
+            image.style.width = '200px'; // Indiquez les dimensions souhaitées ici.
+            image.style.height = 'auto'; // Vous pouvez également utiliser "px" si vous voulez spécifier une hauteur.
+        });
+
+        reader.readAsDataURL(file);
+    }
 }
