e853d8052270fbb322aa28b4eda4e4e6a4882467
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-10-24 09:27:42 +0200

    logout sans cookie

--- html/connexion/logout.php
+++ html/connexion/logout.php
@@ -1,20 +1,13 @@
 <?php
-session_start(); 
+session_start(); // Démarrer la session
 
-$_SESSION = array();
-
-// Si vous souhaitez aussi détruire le cookie de session
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
+// Supprimer toutes les variables de session
+$_SESSION = array(); // Réinitialiser toutes les variables de session
 
 // Détruire la session
-session_destroy();
+session_destroy(); // Met fin à la session active
 
-// Rediriger vers la page de connexion après déconnexion
-header("Location: ../autres_pages/accueil.php");
+// Rediriger vers la page de connexion ou la page d'accueil
+header("Location: connexion.php");
 exit();
+?>
