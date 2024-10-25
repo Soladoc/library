a81ebbd20521011a264cf36d8a77a5eaa47308f1
Scover <thediscover22450@gmail.com>
2024-10-25 10:18:40 +0200

    do

--- sql/vuesBDD.sql
+++ sql/vuesBDD.sql
@@ -5,3 +5,4 @@ create or replace view pro_prive as select * from _professionnel join _prive on 
 create or replace view pro_public as select * from _professionnel join _public on id_professionnel=id_public natural join _compte;
 create or replace view avis as select * from _avis;
 create or replace view tous_comptes_pro as select email,mdp_hash,existe,id_professionnel from _compte natural join _professionnel;
+ 
