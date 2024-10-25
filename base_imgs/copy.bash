9b4e03f80db34aef71e7484b684b0a4039f0f301
Scover <thediscover22450@gmail.com>
2024-10-25 10:14:51 +0200

    nah

--- base_imgs/copy.bash
+++ base_imgs/copy.bash
@@ -4,5 +4,5 @@ set -xeu
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 for f in 1.png 2.png 3.jpg; do
-    docker cp $f web:/var/www/html/images_utilisateur/$f
+    sudo docker cp $f web:/var/www/html/images_utilisateur/$f
 done
