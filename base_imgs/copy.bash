27c8d97303505b25a2ba6cf38114bbf9ae1393fc
Scover <thediscover22450@gmail.com>
2024-10-25 10:16:47 +0200

    mkdir

--- base_imgs/copy.bash
+++ base_imgs/copy.bash
@@ -3,6 +3,8 @@
 set -xeu
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+sudo docker exec web mkdir -p /var/www/html/images_utilisateur
+
 for f in 1.png 2.png 3.jpg; do
     sudo docker cp $f web:/var/www/html/images_utilisateur/$f
 done
