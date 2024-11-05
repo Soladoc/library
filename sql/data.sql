ccc2794b446d4c6cca97e2e6ad87716abe38950f
Scover <thediscover22450@gmail.com>
2024-11-06 00:10:51 +0100

    2eme offre

--- sql/data.sql
+++ sql/data.sql
@@ -10,35 +10,6 @@ values
     ('premium', 10);
 
 insert into
-    _image (taille, mime_subtype, legende)
-values
-    (250244, 'webp', '010717 aquarev 045'),
-    (125156, 'jpeg', '1200x680_grenouille'),
-    (320890, 'webp', 'acro'),
-    (264270, 'webp', 'antenne_pb1_c_cite_des_telecoms'),
-    (8370, 'jpeg', 'arche du temps'),
-    (28511, 'jpeg', 'cap-frehel-lighthouse'),
-    (80529, 'jpeg', 'cdt_vueext_tdrone-141_md1-800x600'),
-    (45395, 'jpeg', 'celtique tour'),
-    (45504, 'jpeg', 'char-a-voile'),
-    (216628, 'webp', 'chimair_cite-des-telecoms-82-scaled'),
-    (65546, 'webp', 'Cite_des_telecoms_Parc_du_Radome_Pleumeur_Bodou_9'),
-    (136726, 'jpeg', 'creperie de l''abbaye de beauport'),
-    (348163, 'jpeg', 'creperie les alize'),
-    (134354, 'jpeg', 'facade-cite-telecoms'),
-    (278752, 'jpeg', 'fort-la-latte'),
-    (112259, 'jpeg', 'laser game'),
-    (165607, 'jpeg', 'le-de-de-tregastel'),
-    (273188, 'jpeg', 'les-oeuvres'),
-    (301557, 'jpeg', 'loisirs_parc_du_radome_cite_des_telecoms_vue_aerienne_alex_kozel'),
-    (292245, 'jpeg', 'maison-d-ernest-renan'),
-    (76107, 'jpeg', 'parc-indian-forest-morieux'),
-    (47305, 'jpeg', 'poesie'),
-    (252164, 'jpeg', 'reserve-naturelle-des'),
-    (258810, 'jpeg', 'st-brieuc-museum-front'),
-    (141400, 'webp', 'vallee-des-saints');
-
-insert into
     pro_prive (siren, denomination, email, mdp_hash, nom, prenom, telephone)
 values
     ('123456789', 'MERTREM Solutions', 'contact@mertrem.org', /*toto*/ '$2y$10$EGLHZkQPfzunBskmjGlv0eTVbF8uot3J6R/W76TIjUw33xSYadike', 'Dephric', 'Max', '0288776655');
@@ -52,6 +23,40 @@ insert into
     membre (pseudo, email, mdp_hash, nom, prenom, telephone)
 values
     ('5cover', 'the.scover@gmail.co', /*toto*/ '$2y$10$EGLHZkQPfzunBskmjGlv0eTVbF8uot3J6R/W76TIjUw33xSYadike', 'Scover', 'NoLastName', '2134657980');
+
+insert into
+    _image(taille, mime_type, legende)
+values
+    (250244, 'webp', '010717 aquarev 045'), -- 1
+    (125156, 'jpeg', '1200x680_grenouille'), -- 2
+    (320890, 'webp', 'acro'), -- 3
+    (264270, 'webp', 'antenne_pb1_c_cite_des_telecoms'), -- 4
+    (8370, 'jpeg', 'arche du temps'), -- 5
+    (28511, 'jpeg', 'cap-frehel-lighthouse'), -- 6
+    (80529, 'jpeg', 'cdt_vueext_tdrone-141_md1-800x600'), -- 7
+    (45395, 'jpeg', 'celtique tour'), -- 8
+    (45504, 'jpeg', 'char-a-voile'), -- 9
+    (216628, 'webp', 'chimair_cite-des-telecoms-82-scaled'), -- 10
+    (65546, 'webp', 'Cite_des_telecoms_Parc_du_Radome_Pleumeur_Bodou_9'), -- 11
+    (136726, 'jpeg', 'creperie de l''abbaye de beauport'), -- 12
+    (348163, 'jpeg', 'creperie les alize'), -- 13
+    (134354, 'jpeg', 'facade-cite-telecoms'), -- 14
+    (278752, 'jpeg', 'fort-la-latte'), -- 15
+    (423934, 'jpeg', 'lasergame-blind-test'), -- 16
+    (471851, 'jpeg', 'lasergame-flechettes'), -- 17
+    (112259, 'jpeg', 'laser game'), -- 18
+    (422847, 'png', 'lasergame-lasergame'), -- 19
+    (489219, 'jpeg', 'lasergame-quiz'), -- 20
+    (165607, 'jpeg', 'le-de-de-tregastel'), -- 21
+    (273188, 'jpeg', 'les-oeuvres'), -- 22
+    (301557, 'jpeg', 'loisirs_parc_du_radome_cite_des_telecoms_vue_aerienne_alex_kozel'), -- 23
+    (292245, 'jpeg', 'maison-d-ernest-renan'), -- 24
+    (76107, 'jpeg', 'parc-indian-forest-morieux'), -- 25
+    (47305, 'jpeg', 'poesie'), -- 26
+    (252164, 'jpeg', 'reserve-naturelle-des'), -- 27
+    (258810, 'jpeg', 'st-brieuc-museum-front'), -- 28
+    (141400, 'webp', 'vallee-des-saints'); -- 29
+
 
 with id_adresse as (
     insert into
@@ -87,5 +92,38 @@ De 3 à 6 joueurs, en famille, entre collègues ou entre amis, venez relever nos
     'Nous vous proposons un escape game avec énigmes',
     'Nous ne vous proposons pas de repas'
 );
+
+with id_adresse as (
+    insert into _adresse
+        (code_insee_commune, nom_voie)
+    values
+        ('22360', 'Zone de loisirs Brezillet ouest')
+    returning id
+), id_offre as (
+    insert into activite (
+        id_professionnel, id_image_principale, id_adresse, libelle_abonnement, url_site_web, titre, resume, description_detaillee,
+        indication_duree, age_requis, prestations_incluses, prestations_non_incluses
+    ) values (
+        1,
+        18,
+        (table id_adresse),
+        'gratuit',
+        'https://saint-brieuc.lasergame-evolution.fr/',
+        'Laser Game Evolution Saint-Brieuc',
+        'Bienvenue au Laser Game Evolution de Saint-Brieuc ! Réservation conseillée - Vous pouvez jouer à partir de 4 joueurs et à partir de 7 ans minimum.',
+        'Equipé de votre PISTOLET 100% LASER, votre précision et votre rapidité sont vos atouts pour surpasser vos adversaires dans nos labyrinthes ! Utilisez les planches pour vous cacher, mais méfiez-vous des meurtrières et des miroirs ! Pour un anniversaire, avec vos amis ou encore vos collaborateurs, venez jouer sur réservation, car nos labyrinthes vous seront entièrement privatisés ! Une équipe dynamique sera à votre disposition pour vous faire passer un moment inoubliable ! Laser Game Evolution, LE JEU LASER 100 % !',
+        '0:20:',
+        7,
+        'Nous vous proposons un laser game, un quiz game, un blindtest, des fléchettes',
+        'Nous ne vous proposons pas de repas'
+    ) returning id
+)
+insert into _gallerie
+    (id_offre, id_image)
+values
+    ((table id_offre), 16),
+    ((table id_offre), 17),
+    ((table id_offre), 19),
+    ((table id_offre), 20);
 
 commit;
