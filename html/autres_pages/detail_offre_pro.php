4ce9bb088b24aad57a0630d7debd494fb135edaf
Scover <thediscover22450@gmail.com>
2024-11-19 11:04:22 +0100

    fix  args

--- html/autres_pages/detail_offre_pro.php
+++ html/autres_pages/detail_offre_pro.php
@@ -4,11 +4,11 @@ require_once 'queries.php';
 require_once 'component/head.php';
 
 
+$args = [
+    'id' => getarg($_GET, 'id', arg_filter(FILTER_VALIDATE_INT))
+];
 // Vérifier si l'ID est présent dans l'URL
 if ($_POST) {
-    $args = [
-        'id' => getarg($_GET, 'id', arg_filter(FILTER_VALIDATE_INT))
-    ];
 
     // Connexion à la base de données
     $pdo = db_connect();
