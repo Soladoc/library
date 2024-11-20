40550745b29db79c56ac8ec901448ff859d81d6d
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-11-20 15:18:52 +0100

    enlever comment

--- html/autres_pages/detail_offre.php
+++ html/autres_pages/detail_offre.php
@@ -175,55 +175,6 @@ $avis=query_avis()
                 <?php } ?>
             </div>
         </section>
-        <!-- User Reviews -->
-        <!--<section class="offer-reviews">
-            <h3>Avis des utilisateurs</h3>
-            <div class="review-form">
-                <label for="text_avis">Mettre un avis</label>
-                <form method="post" action="detail_offre.php">
-                    <textarea type="text" placeholder="Votre avis..." name="text_avis"></textarea>
-                    <label for="rating">Note&nbsp;:</label>
-                    <select id="rating" required>
-                        <option value="5">5 étoiles</option>
-                        <option value="4">4 étoiles</option>
-                        <option value="3">3 étoiles</option>
-                        <option value="2">2 étoiles</option>
-                        <option value="1">1 étoile</option>
-                    </select>
-                    <label for="date">Date de votre visite</label>
-                    <input type="date" id="date" name="date" required>
-                    </br>
-                    <label for="consent">Je certifie que l’avis reflète ma propre expérience et mon opinion sur cette Offre </label>
-                    <input type="checkbox" name="consent" required>
-                    <button class="btn-publish">Publier</button>
-                </form>
-            </div>
-
-            
-            <div class="review-summary">
-                <h4>Résumé des notes</h4>
-                <p>Moyenne&nbsp;: <?php $offre['note_moyenne'] ?>/5 ★</p>
-                <div class="rating-distribution">
-                    <?php $avis = query_avis(id_offre: $offre['id']); ?>
-                    <p>5 étoiles&nbsp;: <?php count(array_filter($avis, fn($a) => $a['note'] === 5)) ?> avis.</p>
-                    <p>4 étoiles&nbsp;: <?php count(array_filter($avis, fn($a) => $a['note'] === 4)) ?> avis.</p>
-                    <p>3 étoiles&nbsp;: <?php count(array_filter($avis, fn($a) => $a['note'] === 3)) ?> avis.</p>
-                    <p>2 étoiles&nbsp;: <?php count(array_filter($avis, fn($a) => $a['note'] === 2)) ?> avis.</p>
-                    <p>1 étoile&nbsp;: <?php count(array_filter($avis, fn($a) => $a['note'] === 1)) ?> avis.</p>
-                </div>
-            </div>
-
-            
-            <div class="review-list">
-                <h4>Avis de la communautée</h4>
-                <?php foreach ($avis as $avis_temp) { ?>
-                    <div class="review">
-                        <p><strong><?php $avis_temp['pseudo'] ?></strong> - <?php $avis_temp['note'] ?>/5</p>
-                        <p><?php $avis_temp['commentaire'] ?></p>
-                    </div>
-                <?php } ?>
-            </div>
-        </section>**/-->
     </main>
     <?php require 'component/footer.php' ?>
 
