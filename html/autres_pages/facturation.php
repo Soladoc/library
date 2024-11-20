45dfabe13d7c6e14e1379b786a1dfecaa33076fc
Benjamin <benjamin.dumontgirard@gmail.com>
2024-11-20 14:14:37 +0100

    mael fais mois des gosses

--- html/autres_pages/facturation.php
+++ html/autres_pages/facturation.php
@@ -6,7 +6,15 @@
 <body>
     <?php require 'component/header.php' ?>
     <main>
-        
+        <?php
+            $OFFRES = query_offres($id_professionnel);
+            while ($offer = $OFFRES_HORS_LIGNE->fetch()) {
+                echo "<pre>";
+                print_r($offer);
+                echo "</pre>";
+            }
+            $OFFRES_HORS_LIGNE->closeCursor()
+        ?>
     </main>
 </body>
 </html>
