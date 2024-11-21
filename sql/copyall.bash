3b381156aeec44ed3a4f1b2e7e6035051d7483b0
Scover <thediscover22450@gmail.com>
2024-11-22 00:51:12 +0100

    there can only be pain

--- sql/copyall.bash
+++ sql/copyall.bash
@@ -3,8 +3,8 @@
 set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-(
+{
     echo 'begin;'
     cat creaBDD.sql fonctions.sql vuesBDD.sql triggers.util.sql triggers/*.sql bigdata.sql data.sql images.sql offre/*.sql
     echo 'commit;'
-) | xsel -ib
+} | xsel -ib
