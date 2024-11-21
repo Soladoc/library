84fbfee3d0a44cfd53b915a5e1d471d95e966155
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-11-21 13:31:45 +0100

    EF

--- html/avis/modifier.php
+++ html/avis/modifier.php
@@ -15,10 +15,6 @@ $id_offre = intval($_GET['offre']);
 
 echo 4;
 
-// Vérification si l'ID de l'avis existe
-//$stmt = db_connect()->prepare("SELECT * FROM pact._avis WHERE id = ?");
-//$stmt->execute([$id_avis]);
-//$avis = $stmt->fetch(PDO::FETCH_ASSOC);
 
 echo 5;
 
