85c8d79f2e8a1c5343600bc4c965a48357a8b79c
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-11-21 16:27:15 +0100

    de

--- html/autres_pages/choix_categorie_creation_offre.php
+++ html/autres_pages/choix_categorie_creation_offre.php
@@ -12,7 +12,7 @@ require_once 'component/head.php';
     <form action="creation_offre.php" method="get">
         <p>Choisissez la catégorie de votre offre</p>
         <button type="submit" name="type_offre" value="spectacle">Spectacle</button>
-        <button type="submit" name="type_offre" value="parc-attraction">Parc d'attraction</button>
+        <button type="submit" name="type_offre" value="parc-attractions">Parc d'attraction</button>
         <button type="submit" name="type_offre" value="visite">Visite</button>
         <button type="submit" name="type_offre" value="restaurant">Restaurant</button>
         <button type="submit" name="type_offre" value="activite">Activité</button>
