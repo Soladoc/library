be4ee88fc61b44f4a193a9fa37566216e5ecb180
Soladoc <romaingrandchamppro@gmail.com>
2024-11-25 14:05:32 +0100

    correction tri

--- html/json/offres.php
+++ html/json/offres.php
@@ -5,6 +5,7 @@ header('Content-Type: application/json; charset=utf-8');
 $offres = iterator_to_array(DB\query_offres(), false);
 $i=0;
 foreach ($offres as &$offre) {
+    $offre['min'] = floatval($offre['min']);
     $offre['formatted_address'] = format_adresse(notfalse(DB\query_adresse($offre['id_adresse'])));
     error_log(strval($i++));
 }
