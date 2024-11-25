71916fdc1990e7bc484dca69b05661b192617c1d
mariuschartier <icelos@yahoo.com>
2024-11-25 14:37:33 +0100

    location_modif_compte

--- html/autres_pages/modif_compte.php
+++ html/autres_pages/modif_compte.php
@@ -92,16 +92,18 @@ if ($_POST) {
     $new_mdp = getarg($_POST, 'new_mdp', null, false);
     $confirmation_mdp = getarg($_POST, 'confirmation_mdp', null, false);
     $old_mdp = getarg($_POST, 'old_mdp', null, false);
-
-    if ($new_mdp && password_verify($old_mdp, $mdp_hash)) {
+    if(old_mdp){
+        if ($new_mdp && password_verify($old_mdp, $mdp_hash)) {
         if ($confirmation_mdp === $new_mdp) {
             DB\query_uptate_mdp($id, password_hash($new_mdp)); 
         } else {
-            redirect_to(location_connexion('Mot de passe de confirmation different.'));
+            redirect_to(location_modif_compte('Mot de passe de confirmation different.'));
         }
-    } else {
-        redirect_to(location_connexion(error: 'Mot de passe incorrect.'));
+        } else {
+            redirect_to(location_modif_compte(error: 'Mot de passe incorrect.'));
+        }
     }
+    
 }
 
 ?>
