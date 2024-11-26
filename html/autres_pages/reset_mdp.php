0dabc60a5daf0807e9d0e82b10ded9eb5f40e07c
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-11-26 14:15:28 +0100

    reset_mdp

---
+++ html/autres_pages/reset_mdp.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'component/Page.php';
+require_once 'util.php';
+
+$page = new Page('Connexion');
+
+$return_url = getarg($_GET, 'return_url', required: false);
+$error = getarg($_GET, 'error', required: false);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php $page->put_head() ?>
+
+<body>
+<?php $page->put_header() ?>
+<main>
+    <h1>Connexion</h1>
+    <section class="connexion">
+        <div class="champ-connexion">
+            <br>
+            <!-- Formulaire de connexion -->
+            <form action="/connexion/login.php" method="POST">
+                <div class="champ">
+                    <label for="login">Adresse e-mail ou pseudo *</label>
+                    <input id="login" name="login" type="text" placeholder="exemple@mail.fr" required>
+                </div>
+                <br>
+                <?php if ($error !== null) { ?>
+                    <p class="error"><?= htmlspecialchars($error) ?></p>
+                <?php } ?>
+                <button type="submit" class="btn-connexion">Se connecter</button>
+                <?php if ($return_url !== null) { ?>
+                    <input type="hidden" name="return_url" value="<?= htmlspecialchars($return_url) ?>">
+                <?php } ?>
+            </form>
+            <br><br>
+            <a href="connexion.php" class="btn-creer">Retour</a>
+            <br>
+        </div>
+    </section>
+</main>
+<?php $page->put_footer() ?>
+</body>
+
+</html>
