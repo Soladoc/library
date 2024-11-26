deae1370e6ca81a527a962db6f52927145f5329d
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-11-26 14:34:10 +0100

    enfin

--- html/autres_pages/reset_mdp.php
+++ html/autres_pages/reset_mdp.php
@@ -2,7 +2,7 @@
 require_once 'component/Page.php';
 require_once 'util.php';
 
-$page = new Page('Connexion');
+$page = new Page('Reinitialiser le mot de passe');
 
 $return_url = getarg($_GET, 'return_url', required: false);
 $error = getarg($_GET, 'error', required: false);
