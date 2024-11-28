a5e7a268cd77f1f4f9c7b109bf40b5b30c97671b
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-11-28 11:52:42 +0100

    zdefr

--- include/queries.php
+++ include/queries.php
@@ -271,7 +271,8 @@ function insert_uploaded_image(array $img, ?string $legende = null): array
  * @return bool `true` si un professionnel privé d'id $id_pro_prive existe, `false` sinon.
  */
 function exists_pro_prive(int $id_pro_prive): bool
-{
+{   
+    var_dump($id_pro_prive);
     $stmt = notfalse(connect()->prepare('select ? in (select id from pro_prive)'));
     bind_values($stmt, [1 => [$id_pro_prive, PDO::PARAM_INT]]);
     $a=$stmt->fetchColumn();
