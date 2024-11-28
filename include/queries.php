7ac2c366c5fd5c0f187ebc1f78a6133a2f4cb14b
Scover <thediscover22450@gmail.com>
2024-11-28 11:55:50 +0100

    tentative

--- include/queries.php
+++ include/queries.php
@@ -273,7 +273,7 @@ function insert_uploaded_image(array $img, ?string $legende = null): array
 function exists_pro_prive(int $id_pro_prive): bool
 {   
     var_dump($id_pro_prive);
-    $stmt = notfalse(connect()->prepare('select ? in (select id from pro_prive)'));
+    $stmt = notfalse(connect()->prepare('select exists(select from pro_prive where id = ?)'));
     bind_values($stmt, [1 => [$id_pro_prive, PDO::PARAM_INT]]);
     $a=$stmt->fetchColumn();
     var_dump($a);
