98654e4031f154a513cd33f4eddd0c8bf2b325db
Scover <thediscover22450@gmail.com>
2024-11-29 11:23:59 +0100

    g

--- html/autres_pages/accueil.php
+++ html/autres_pages/accueil.php
@@ -3,18 +3,6 @@ require_once 'component/offre.php';
 require_once 'component/Page.php';
 
 $page = new Page('Accueil');
-
-
-$recherche = null;
-
-$valider = getarg($_GET,"valider",required: false);
-$mot_cle= getarg($_GET,"mot_cle",required: false);
-$modif_affichage = false;
-
-if ($valider && !empty($mot_cle)) {
-    $modif_affichage = true;
-    $mot_cle= getarg($_GET,"mot_cle");
-}
 ?>
 
 <!DOCTYPE html>
@@ -29,15 +17,12 @@ if ($valider && !empty($mot_cle)) {
         <section class="search-section">
             <h1>Accueil</h1>
             <br>
-            <form action="" name="bare_de_recherche" method="get" >
-
-                <input type="text" name="mot_cle" placeholder=">Mots-clés"  value="<?php echo $mot_cle ?>">
-                <input type="submit" name="valider" value="Recherche" class="btn-search">
-            </form>
-                <!-- <input type="text" placeholder="Rechercher des activités, restaurants, spectacles...">
+            <div class="search-bar">
+                <input type="text" placeholder="Rechercher des activités, restaurants, spectacles...">
                 <a href="recherche.php">
                     <button class="btn-search">Rechercher</button>
-                </a> -->
+                </a>
+            </div>
         </section>
 
         <!-- Section des offres à la une -->
@@ -45,14 +30,8 @@ if ($valider && !empty($mot_cle)) {
             <h2>Offres à la une</h2>
             <div class="offer-list">
                 <?php
-                if ($modif_affichage) {
-                    $offres = DB\query_select_offre_motcle($mot_cle);
-                }
-                else {
-                    // Préparer et exécuter la requête SQL pour récupérer toutes les offres
+                // Préparer et exécuter la requête SQL pour récupérer toutes les offres
                 $offres = DB\query_offres_a_une();
-                }
-                
 
                 // Boucler sur les résultats pour afficher chaque offre
                 foreach ($offres as $offre) {
