0258aff5b5e92561a17325aa15927ea7f542cdec
Scover <thediscover22450@gmail.com>
2024-11-29 11:41:56 +0100

    m

--- include/component/offre.php
+++ include/component/offre.php
@@ -5,6 +5,20 @@ require_once 'redirect.php';
 require_once 'util.php';
 
 /**
+ * Affiche le composant d'image utilisateur.
+ * @param array $image L'image à afficher (ligne issue la BDD, voir `DB\query_image`)
+ * @return void
+ */
+function put_image(array $image)
+{
+?>
+<img src="<?= "/images_utilisateur/{$image['id']}.{$image['mime_subtype']}" ?>"
+    alt="<?= $image['legende'] ?: "Image de l'offre" ?>"
+    title="<?= $image['legende'] ?>">
+<?php
+}
+
+/**
  * Affiche le composant de carte d'offfre pour professionnel
  * @param array<string, mixed> $offre L'offre à afficher (ligne issue la BDD, foir `DB\query_offre`)
  * @return void
