d57eedd22123e4d0c8b04cb07cc6019014aec422
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-11-29 13:42:38 +0100

    essai tag

--- html/autres_pages/modif_offre.php
+++ html/autres_pages/modif_offre.php
@@ -237,7 +237,7 @@ if ($_POST) {
                 // Récupération des tags associés à l'offre
                 $tags_remplis = array_column(DB\query_tags($offre['id']), 'tag');
                 $tags_disponibles = $args['type_offre'] === 'restaurant' ? TAGS_RESTAURANT : DEFAULT_TAGS;
-
+                print_r($tags_remplis);
                 foreach ($tags_disponibles as $tag) {
                     // Si le tag est déjà dans les tags remplis, ajoutez 'checked'
                     $checked = in_array($tag, $tags_remplis) ? 'checked' : '';
