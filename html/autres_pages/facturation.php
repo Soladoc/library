1422a16dede537a4e8cb5d343fd78eb757e7cc2a
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-11-29 16:52:28 +0100

    +info facturation

--- html/autres_pages/facturation.php
+++ html/autres_pages/facturation.php
@@ -21,7 +21,10 @@ $id_professionnel = Auth\exiger_connecte_pro();
         $offres = DB\query_offres($id_professionnel);
         foreach ($offres as $offre) {
             echo '<pre>';
+            print_r($offre['titre']);
             print_r($offre['libelle_abonnement']);
+            print_r($offre['categorie']);
+            print_r($offre['en_ligne_ce_mois_pendant']);
             // cc benjamin, j'ai renommé l'attribut prix de abonnement en prix_jouranlier
             // ce message s'autodétruira dans 5 réinitialisations de BDD
             echo '</pre>';
