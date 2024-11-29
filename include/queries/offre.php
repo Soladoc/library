10a25d01af105c80f32aa7e49e7020e3d7955691
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-11-29 14:27:31 +0100

    ohohoh

--- include/queries/offre.php
+++ include/queries/offre.php
@@ -34,7 +34,7 @@ function query_offres(?int $id_professionnel = null, ?bool $en_ligne = null): It
 
 function query_offres_a_une(): Iterator
 {
-    $stmt = notfalse(connect()->prepare('SELECT * FROM _offre WHERE libelle_abonnement = "premium" ORDER BY RAND() LIMIT 5;'));
+    $stmt = notfalse(connect()->prepare('SELECT * FROM _offre WHERE _offre.libelle_abonnement = "premium" ORDER BY RAND() LIMIT 5;'));
     notfalse($stmt->execute());
     return $stmt->getIterator();
 }
