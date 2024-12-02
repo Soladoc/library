252559226bab202cfedce0b8ae5174951ca9ef1d
Soladoc <romaingrandchamppro@gmail.com>
2024-12-02 16:18:35 +0100

    date ouverture (commentaire)

--- html/json/offres.php
+++ html/json/offres.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'queries.php';
+require_once 'MultiRange.php';
 header('Content-Type: application/json; charset=utf-8');
 
 $offres = iterator_to_array(DB\query_offres(), false);
@@ -7,6 +8,7 @@ foreach ($offres as &$offre) {
     $offre['prix_min'] = floatval($offre['prix_min']);
     $offre['formatted_address'] = format_adresse(notfalse(DB\query_adresse($offre['id_adresse'])));
     $offre['tags'] = DB\query_tags($offre['id']);
+    //$offre['periodes_ouverture']=MultiRange::parse($offre['periodes_ouverture'],FiniteTimestamp::parse(...));
 }
 echo notfalse(json_encode($offres));
 ?>
