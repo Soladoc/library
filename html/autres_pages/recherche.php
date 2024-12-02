ef39b86c070b83ead84eb8959be3da55909ddb5f
marius <icelos@yahoo.com>
2024-12-02 11:47:16 +0100

    import js

--- html/autres_pages/recherche.php
+++ html/autres_pages/recherche.php
@@ -22,15 +22,17 @@ if ($valider && !empty($mot_cle)) {
 <html lang="fr">
 <?php $page->put_head() ?>
 <body>
+<script src="script_js\tri_recherche.js"></script>
     <?php $page->put_header() ?>
     <main>
         <?php require_once 'util.php'; require_once 'queries.php'; dbg_print(DB\query_tags(11));?>
+
         <section class="search-section">
             <h1>Recherche</h1>
             <br>
             <div class="search-bar">
                 <!-- <input id="barre-recherche" type="text" placeholder="Rechercher des activités, restaurants, spectacles..."> -->
-                <input type="text" id="keyword-search" placeholder="Rechercher par mot-clé"oninput="filterOffers()">
+                <input type="text" id="keyword-search" placeholder="Rechercher par mot-clé" oninput="filterOffers()">
                 
             </div>
         </section>
