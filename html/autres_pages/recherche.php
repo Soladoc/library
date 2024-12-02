ed0bed9adeda402f86834ccb24b37fb43b3c28fa
Soladoc <romaingrandchamppro@gmail.com>
2024-12-02 11:44:04 +0100

    fermeture offre

--- html/autres_pages/recherche.php
+++ html/autres_pages/recherche.php
@@ -24,7 +24,6 @@ if ($valider && !empty($mot_cle)) {
 <body>
     <?php $page->put_header() ?>
     <main>
-        <?php require_once 'util.php'; require_once 'queries.php'; dbg_print(DB\query_tags(11));?>
         <section class="search-section">
             <h1>Recherche</h1>
             <br>
