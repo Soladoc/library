943ba030dae4a14c641b26c550e87725243c3580
Scover <thediscover22450@gmail.com>
2024-12-03 20:59:22 +0100

    yipeee

--- include/component/DynamicTable.php
+++ include/component/DynamicTable.php
@@ -76,9 +76,13 @@ final class DynamicTable extends Input
     </thead>
     <tbody>
         <?php foreach ($current ?? $this->initial_rows as $row) {
-            ($this->put_row)($this, $row);
+            ?>
+            <tr>
+                <?php ($this->put_row)($this, $row); ?>
+                <button class="button-remove-row" type="button">-</button>
+            </tr>
+            <?php
         } ?>
-        <button class="button-remove-row" type="button">-</button>
     </tbody>
     <tfoot>
         <tr>
