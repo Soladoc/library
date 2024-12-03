861846a26982f2057ca1530dfc5e6daaef389271
Soladoc <romaingrandchamppro@gmail.com>
2024-12-03 11:38:18 +0100

    test

--- html/autres_pages/detail_offre_pro.php
+++ html/autres_pages/detail_offre_pro.php
@@ -32,11 +32,9 @@ if ($offre) {
     $site_web = $offre['url_site_web'];
     $image_pricipale = notfalse(Image::from_db($offre['id_image_principale']));
     $en_ligne = $offre['en_ligne'];
-    if ($en_ligne!=1) {
-        $en_ligne = 0;
+    if ($en_ligne) {
+        echo 'en ligne';
     }
-    echo $offre['en_ligne'];
-    echo $en_ligne;
     $info_adresse = DB\query_adresse($adresse);
     $avis = DB\query_avis();
     $galerie = DB\query_galerie($args['id']);
