f540417d147d9ae5e9fd1543bc3b1889045182ad
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-12-04 09:50:55 +0100

    frrf

--- html/autres_pages/accueil.php
+++ html/autres_pages/accueil.php
@@ -4,17 +4,6 @@ require_once 'component/CarteOffre.php';
 require_once 'model/Offre.php';
 
 $page = new Page('Accueil');
-
-
-
-$valider = getarg($_GET,"valider",required: false);
-$mot_cle= getarg($_GET,"mot_cle",required: false);
-$modif_affichage = false;
-
-if ($valider && !empty($mot_cle)) {
-    $modif_affichage = true;
-    $mot_cle= getarg( $_GET,"mot_cle");
-}
 ?>
 
 <!DOCTYPE html>
