3be0e6baee0bb3486ad480896cd1cff38c98bec6
Scover <thediscover22450@gmail.com>
2024-12-04 15:37:20 +0100

    rem

--- include/db.php
+++ include/db.php
@@ -30,7 +30,6 @@ function connect(): PDO
 
     // Connect to the database
     $driver = 'pgsql';
-    dbg_print(_is_localhost());
     [$host, $port, $dbname, $username, $password] = _is_localhost() 
         ? ['localhost',
             5432,
