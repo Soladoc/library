c11f508a54d78a97a7f4b177f3246878bee60f4f
Xx-Nasank-xX <maelanpotier05@gmail.com>
2024-12-04 09:45:44 +0100

    co

--- html/autres_pages/accueil.php
+++ html/autres_pages/accueil.php
@@ -42,7 +42,7 @@ if ($valider && !empty($mot_cle)) {
             <div class="offer-list">
                 <?php
 
-                $offres = $modif_affichage ? Offre::from_db_by_motcle($mot_cle) : Offre::from_db_a_la_une();
+                $offres =  Offre::from_db_a_la_une();
                 
                 // Préparer et exécuter la requête SQL pour récupérer toutes les offres
 
