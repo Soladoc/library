ade3882c3b8e2c12f4c0f06f5fcced0a9439f3bd
Scover <thediscover22450@gmail.com>
2024-12-04 06:34:50 +0100

    yah let's just makes this easier for ourselves

--- html/autres_pages/modif_offre.php
+++ html/autres_pages/modif_offre.php
@@ -9,22 +9,21 @@ require_once 'component/Page.php';
 require_once 'component/InputDuree.php';
 require_once 'model/ProfessionnelPrive.php';
 
-$page = new Page("Modifier offre",
+$page = new Page('Modifier offre',
     ['creation_offre.css'],
     ['module/creation_offre.js' => 'defer type="module"']);
 
 $id_professionnel = Auth\exiger_connecte_pro();
-$est_prive = ProfessionnelPrive::exists($id_professionnel);
+$est_prive        = ProfessionnelPrive::exists($id_professionnel);
 
 $categorie = getarg($_GET, 'categorie', arg_check(f_is_in(array_keys(CATEGORIES_OFFRE))));
-$id_offre = getarg($_GET, 'id', arg_int());
+$id_offre  = getarg($_GET, 'id', arg_int());
 
 $input_offre = new InputOffre(
     $categorie,
     Professionnel::from_db(Auth\exiger_connecte_pro()),
     form_id: 'f',
 );
-
 
 if ($_POST) {
     $offre = $input_offre->get($_POST, $id_offre);
@@ -50,10 +49,3 @@ if ($_POST) {
 </body>
 
 </html>
-<?php
-function put_input_indication_duree(Duree $duree)
-{
-    ?>
-        <label>Durée estimée&nbsp;: <?php (new InputDuree('f', 'indication_duree', 'indication_duree_'))->put() ?></label>
-        <?php
-}
