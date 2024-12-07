13ed04a8089f9f50f4c2149190185666ba963aed
SnoozyUWU <136280725+SnoozyUWU@users.noreply.github.com>
2024-12-07 17:21:45 +0100

    detail_offre

--- html/autres_pages/detail_offre.php
+++ html/autres_pages/detail_offre.php
@@ -121,7 +121,6 @@ if ($_POST) {
                     <?php 
                             elseif (isset($success_message)): ?>
                         <p class="success"><?= htmlspecialchars($success_message) ?></p>
-                        
                     <?php endif ?>
                     <script>
                         //que l'avis ai été publié ou non pares l'affichage du message d'erreur (ou de succes) je scrolle au formulaire de création d'avis
