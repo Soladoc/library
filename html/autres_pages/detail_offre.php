6d855754c9a634b2454aee08559f486ab897ee69
SnoozyUWU <136280725+SnoozyUWU@users.noreply.github.com>
2024-12-07 17:58:10 +0100

    je remet start

--- html/autres_pages/detail_offre.php
+++ html/autres_pages/detail_offre.php
@@ -125,7 +125,7 @@ if ($_POST) {
                     <script>
                         const target = document.querySelector(".review-form");
                         if (target) {
-                            target.scrollIntoView({ behavior: "instant" , block: "end"});
+                            target.scrollIntoView({ behavior: "instant" , block: "start"});
                         }else{
                             console.log("pas de target");
                         }
