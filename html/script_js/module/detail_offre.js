93cf1396f936821e0dc2e2a7345fec9d0e8a3d90
Scover <thediscover22450@gmail.com>
2024-12-08 12:11:48 +0100

    improve interactivity

--- html/script_js/module/detail_offre.js
+++ html/script_js/module/detail_offre.js
@@ -1,6 +1,6 @@
 import { scroller } from './util.js'
 
-const message = document.getElementById('review-form').getElementsByClassName('message').item(0);
-if (message) {
-    scroller(message);
+const review_form = document.getElementById('review-form');
+if (review_form.getElementsByClassName('message').item(0).childElementCount > 0) {
+    scroller(review_form);
 }
