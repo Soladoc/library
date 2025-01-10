bf50903582135b9507c371bfbd8f53b8a56b79fd
Benjamin <benjamin.dumontgirard@gmail.com>
2025-01-10 15:00:52 +0100

    update facturation

--- html/autres_pages/facturation.php
+++ html/autres_pages/facturation.php
@@ -1,8 +1,12 @@
 <?php
+
+use function DB\query_tarif;
+
 require_once 'component/Page.php';
 require_once 'auth.php';
 require_once 'queries/offre.php';
 require_once 'model/Duree.php';
+require_once 'queries.php';
 
 $page = new Page('Facturation');
 
@@ -15,13 +19,14 @@ $page->put(function () {
         echo '<pre>';
         print_r($offre['titre'].' | ' );
         print_r($offre['libelle_abonnement'].' | ' );
+        print_r(query_tarif($offre['libelle_abonnement']).' | ');
         print_r($offre['categorie'].' | ' );
         print_r(Duree::parse($offre['en_ligne_ce_mois_pendant'])->days.' | ' );
-        ;
-        $resO = Duree::parse($offre['en_ligne_ce_mois_pendant'])->days * 1;// le 1 sera le prix de l'abonnement a terme.
+
+        $resO = Duree::parse($offre['en_ligne_ce_mois_pendantc '])->days * query_tarif($offre['libelle_abonnement']);// le 1 sera le prix de l'abonnement a terme.
         $resO += $resO * 0.20;
         $resG += $resO; 
-        print_r($resO.'€ TTC');
+        print_r($resO.' € TTC');
         echo '</br';
         echo '</pre>';
     }
