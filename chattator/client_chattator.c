ada1a283a99d51bba06a241f266395bb4c42bd24
Scover <bardini.raphael@gmail.com>
2025-01-10 10:46:37 +0100

    fmt

--- chattator/client_chattator.c
+++ chattator/client_chattator.c
@@ -1,14 +1,14 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
-int main(){
+int main() {
     int sock;
     int ret;
     struct sockaddr_in addr;
@@ -26,8 +26,7 @@ int main(){
         printf("Connexion impossible.(Code 404)\n");
         perror("connect");
         _exit(EXIT_FAILURE);
-    }
-    else {
+    } else {
         printf("Connexion établie avec le serveur.(Code 200)\n");
         printf("Bienvenue dans votre espace de chat.\n");
     }
@@ -51,22 +50,19 @@ int main(){
             bytes_read = read(sock, buffer, sizeof(buffer) - 1);
             buffer[bytes_read] = '\0';
             printf("%s", buffer);
-        }
-        else if (option == 2) {
+        } else if (option == 2) {
             printf("Entrez votre message :\n");
             fgets(message, sizeof(message), stdin);
             message[strcspn(message, "\n")] = 0;
             write(sock, message, strlen(message));
             fflush(stdout);
-        }
-        else if (option == 3) {
+        } else if (option == 3) {
             printf("Entrez le numéro du message à supprimer :\n");
             scanf("%d", &num_message);
             getchar();
             write(sock, &num_message, sizeof(num_message));
             fflush(stdout);
-        }
-        else if (option == 4) {
+        } else if (option == 4) {
             printf("Entrez le numéro du message à modifier :\n");
             scanf("%d", &num_message);
             getchar();
@@ -77,25 +73,21 @@ int main(){
             fflush(stdout);
             write(sock, message, strlen(message));
             fflush(stdout);
-        }
-        else if (option == 5) {
+        } else if (option == 5) {
             printf("Entrez le nom de l'utilisateur à bloquer :\n");
             fgets(message, sizeof(message), stdin);
             message[strcspn(message, "\n")] = 0;
             write(sock, message, strlen(message));
             fflush(stdout);
-        }
-        else if (option == 6) {
+        } else if (option == 6) {
             printf("Entrez le nom de l'utilisateur à débloquer :\n");
             fgets(message, sizeof(message), stdin);
             message[strcspn(message, "\n")] = 0;
             write(sock, message, strlen(message));
             fflush(stdout);
-        }
-        else if (option == 7) {
+        } else if (option == 7) {
             printf("Entrez le nom de l'utilisateur à débloquer :\n");
-        }
-        else if (option == 8) {
+        } else if (option == 8) {
             memset(buffer, 0, sizeof(buffer));
             bytes_read = read(sock, buffer, sizeof(buffer) - 1);
             if (bytes_read > 0) {
--- chattator/serveur_chattator.c
+++ chattator/serveur_chattator.c
@@ -1,16 +1,16 @@
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
+#include <string.h>
 #include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(){
-    int sock,cnx,option,ret,size,opt;
+int main() {
+    int sock, cnx, option, ret, size, opt;
     struct sockaddr_in addr;
     struct sockaddr_in conn_addr;
     char reponse[30];
@@ -47,56 +47,56 @@ int main(){
     FD_SET(cnx, &readfds);
     timeout.tv_sec = 5;
     timeout.tv_usec = 0;
-    while(1) {
+    while (1) {
         ret = select(cnx + 1, &readfds, NULL, NULL, &timeout);
         if (ret == -1) {
-    perror("select()");
-} else if (ret == 0) {
-    printf("Timeout waiting for data.\n");
-} else {
-    // The socket is ready to read, proceed with read()
-    bytes_read = read(cnx, &option, sizeof(option));
-    if (bytes_read > 0) {
-        printf("Received option: %d\n", option);
-    } else {
-        printf("Error reading option, bytes_read: %zd\n", bytes_read);
-    }
-}
+            perror("select()");
+        } else if (ret == 0) {
+            printf("Timeout waiting for data.\n");
+        } else {
+            // The socket is ready to read, proceed with read()
+            bytes_read = read(cnx, &option, sizeof(option));
+            if (bytes_read > 0) {
+                printf("Received option: %d\n", option);
+            } else {
+                printf("Error reading option, bytes_read: %zd\n", bytes_read);
+            }
+        }
 
         if (bytes_read <= 0) {
             close(cnx);
             break;
         }
 
-        switch(option) {
-            case 1:
-                snprintf(reponse, sizeof(reponse), "AFFICHAGE MESSAGES\r\n");
-                break;
-            case 2:
-                snprintf(reponse, sizeof(reponse), "MESSAGE ENVOYE\r\n");
-                break;
-            case 3:
-                snprintf(reponse, sizeof(reponse), "MESSAGE SUPPRIME\r\n");
-                break;
-            case 4:
-                snprintf(reponse, sizeof(reponse), "MESSAGE MODIFIE\r\n");
-                break;
-            case 5:
-                snprintf(reponse, sizeof(reponse), "UTILISATEUR BLOQUE\r\n");
-                break;
-            case 6:
-                snprintf(reponse, sizeof(reponse), "UTILISATEUR DEBLOQUE\r\n");
-                break;
-            case 7:
-                snprintf(reponse, sizeof(reponse), "RECUPERATION MESSAGES\r\n");
-                break;
-            case 8:
-                snprintf(reponse, sizeof(reponse), "Au revoir.\r\n");
-            default:
-                snprintf(reponse, sizeof(reponse), "Commande inconnue\r\n");
+        switch (option) {
+        case 1:
+            snprintf(reponse, sizeof(reponse), "AFFICHAGE MESSAGES\r\n");
+            break;
+        case 2:
+            snprintf(reponse, sizeof(reponse), "MESSAGE ENVOYE\r\n");
+            break;
+        case 3:
+            snprintf(reponse, sizeof(reponse), "MESSAGE SUPPRIME\r\n");
+            break;
+        case 4:
+            snprintf(reponse, sizeof(reponse), "MESSAGE MODIFIE\r\n");
+            break;
+        case 5:
+            snprintf(reponse, sizeof(reponse), "UTILISATEUR BLOQUE\r\n");
+            break;
+        case 6:
+            snprintf(reponse, sizeof(reponse), "UTILISATEUR DEBLOQUE\r\n");
+            break;
+        case 7:
+            snprintf(reponse, sizeof(reponse), "RECUPERATION MESSAGES\r\n");
+            break;
+        case 8:
+            snprintf(reponse, sizeof(reponse), "Au revoir.\r\n");
+        default:
+            snprintf(reponse, sizeof(reponse), "Commande inconnue\r\n");
         }
         write(cnx, reponse, strlen(reponse));
-        if (option==8){
+        if (option == 8) {
             write(cnx, reponse, strlen(reponse));
             close(cnx);
             break;
