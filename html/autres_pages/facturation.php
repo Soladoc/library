8f5488ce5b590bd684ee220e78140d8c55514c27
Benjamin <benjamin.dumontgirard@gmail.com>
2025-01-10 11:03:41 +0100

    pagination Facturation

--- html/autres_pages/facturation.php
+++ html/autres_pages/facturation.php
@@ -10,12 +10,13 @@ $page->put(function () {
     $offres = DB\query_offres($id_professionnel);
     foreach ($offres as $offre) {
         echo '<pre>';
-        print_r($offre['titre']);
-        print_r($offre['libelle_abonnement']);
-        print_r($offre['categorie']);
-        print_r($offre['en_ligne_ce_mois_pendant']);
+        print_r($offre['titre'].'' );
+        print_r($offre['libelle_abonnement'].'' );
+        print_r($offre['categorie'].'' );
+        print_r($offre['en_ligne_ce_mois_pendant'].'' );
         // cc benjamin, j'ai renommé l'attribut prix de abonnement en prix_jouranlier
         // ce message s'autodétruira dans 5 réinitialisations de BDD
+        echo '</br';
         echo '</pre>';
     }
 });
