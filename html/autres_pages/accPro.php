35086b8148dde195cf223860d2eab20f65d424a8
Benjamin <benjamin.dumontgirard@gmail.com>
2025-01-13 11:30:28 +0100

    mb je réparre ca au moins

--- html/autres_pages/accPro.php
+++ html/autres_pages/accPro.php
@@ -15,7 +15,7 @@ $page->put(function () {
     ?>
 
     <h1>Accueil Professionnel</h1>
-    <a class="btn-more-info" href="<?= location_creation_offre() ?>" class="bouton_principale_pro">Créer une offre</a>
+    <a class="btn-more-info" href="<?= location_creation_offre() ?>" class="bouton_principale_pro" id='bouton_creer_offre' >Créer une offre</a>
     <a class="btn-more-info" href="<?= location_facturation() ?>" class="bouton_principale_pro">Facturation</a>
 
     <h3 class="nb-offres"><?= $nb_offres ?> offres</h3>
