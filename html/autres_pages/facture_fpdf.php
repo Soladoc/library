dffb3444f660f13c0606a617e16e80df8c1e1789
marius <icelos@yahoo.com>
2025-01-14 17:44:53 +0100

    new table

--- html/autres_pages/facture_fpdf.php
+++ html/autres_pages/facture_fpdf.php
@@ -37,68 +37,151 @@ class FacturePDF extends FPDF {
         $this->Cell(0, 10, 'Page ' . $this->PageNo() . '/{nb}', 0, 0, 'C'); // Numéro de page
     }
 
-    // Table des produits/services
-//     function Table($header, $data)//ImprovedTable
-// {
-//     // Column widths
-//     $w = array(40, 35, 40, 45);
-//     // Header
-//     for($i=0;$i<count($header);$i++)
-//         $this->Cell($w[0],7,$header[$i],1,0,'C');
+    function Row($data)
+    {
+        // Calculate the height of the row
+        $nb = 0;
+        for($i=0;$i<count($data);$i++)
+            $nb = max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+        $h = 5*$nb;
+        // Issue a page break first if needed
+        $this->CheckPageBreak($h);
+        // Draw the cells of the row
+        for($i=0;$i<count($data);$i++)
+        {
+            $w = $this->widths[$i];
+            $a = isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+            // Save the current position
+            $x = $this->GetX();
+            $y = $this->GetY();
+            // Draw the border
+            $this->Rect($x,$y,$w,$h);
+            // Print the text
+            $this->MultiCell($w,5,$data[$i],0,$a);
+            // Put the position to the right of the cell
+            $this->SetXY($x+$w,$y);
+        }
+        // Go to the next line
+        $this->Ln($h);
+    }
+
+    function CheckPageBreak($h)
+    {
+        // If the height h would cause an overflow, add a new page immediately
+        if($this->GetY()+$h>$this->PageBreakTrigger)
+            $this->AddPage($this->CurOrientation);
+    }
+
+    function NbLines($w, $txt)
+    {
+        // Compute the number of lines a MultiCell of width w will take
+        if(!isset($this->CurrentFont))
+            $this->Error('No font has been set');
+        $cw = $this->CurrentFont['cw'];
+        if($w==0)
+            $w = $this->w-$this->rMargin-$this->x;
+        $wmax = ($w-2*$this->cMargin)*1000/$this->FontSize;
+        $s = str_replace("\r",'',(string)$txt);
+        $nb = strlen($s);
+        if($nb>0 && $s[$nb-1]=="\n")
+            $nb--;
+        $sep = -1;
+        $i = 0;
+        $j = 0;
+        $l = 0;
+        $nl = 1;
+        while($i<$nb)
+        {
+            $c = $s[$i];
+            if($c=="\n")
+            {
+                $i++;
+                $sep = -1;
+                $j = $i;
+                $l = 0;
+                $nl++;
+                continue;
+            }
+            if($c==' ')
+                $sep = $i;
+            $l += $cw[$c];
+            if($l>$wmax)
+            {
+                if($sep==-1)
+                {
+                    if($i==$j)
+                        $i++;
+                }
+                else
+                    $i = $sep+1;
+                $sep = -1;
+                $j = $i;
+                $l = 0;
+                $nl++;
+            }
+            else
+                $i++;
+        }
+        return $nl;
+    }
+
+    function Table($header, $data) {
+        // Largeurs des colonnes
+        $this->widths = [50, 50, 40, 30, 30]; // Largeurs des colonnes
+        $this->aligns = ['L', 'L', 'L', 'C', 'R']; // Alignement des colonnes
+    
+        // Afficher les en-têtes
+        $this->SetFont('Arial', 'B', 12); // Police pour les en-têtes
+        foreach ($header as $i => $col) {
+            $this->Cell($this->widths[$i], 7, utf8_decode($col), 1, 0, 'C');
+        }
+        $this->Ln();
+    
+        // Afficher les données
+        $this->SetFont('Arial', '', 12); // Police normale pour les données
+        foreach ($data as $row) {
+            $this->Row(array_map('utf8_decode', $row));
+        }
+    }
+// function Table($header, $data) {
+//     // Largeurs des colonnes
+//     $w = [50, 50, 40, 30, 30]; // Largeur pour chaque colonne : Titre, Abonnement, Catégorie, Jours, Prix
+//     $this->SetFont('Arial', 'B', 12); // Police pour les en-têtes
+
+//     // Afficher les en-têtes
+//     foreach ($header as $i => $col) {
+//         $this->Cell($w[$i], 7, utf8_decode($col), 1, 0, 'C'); // En-têtes centrées
+//     }
 //     $this->Ln();
-//     // Data
-//     foreach($data as $row)
-//     {
-//         $this->Cell($w[0],6,$row[0],'LR');
-//         $this->Cell($w[1],6,$row[1],'LR');
-//         $this->Cell($w[1],6,$row[2],'LR');
-//         $this->Cell($w[1],6,$row[3],'LR');
-//         $this->Cell($w[2],6,number_format($row[4]),'LR',0,'R');
-//         // $this->Cell($w[3],6,number_format($row[5]),'LR',0,'R');
+
+//     // Données
+//     $this->SetFont('Arial', '', 12); // Police normale pour les données
+//     foreach ($data as $row) {
+//         // Colonne 1 : Titre avec MultiCell pour gérer les noms longs
+//         $x = $this->GetX(); // Position X courante
+//         $y = $this->GetY(); // Position Y courante
+//         $this->MultiCell($w[0], 6, utf8_decode($row[0]), 'LR'); // MultiCell gère les retours à la ligne
+//         $this->SetXY($x + $w[0], $y); // Ajuste la position pour les colonnes suivantes
+
+//         // Colonne 2 : Type d'abonnement
+//         $this->Cell($w[1], 6, utf8_decode($row[1]), 1, 0, 'C');
+
+//         // Colonne 3 : Catégorie
+//         $this->Cell($w[2], 6, utf8_decode($row[2]), 1, 0, 'C');
+
+//         // Colonne 4 : Jours
+//         $this->Cell($w[3], 6, $row[3], 1, 0, 'C');
+
+//         // Colonne 5 : Prix TTC
+//         $this->Cell($w[4], 6, number_format($row[4], 2, ',', ' ') . ' €', 1, 0, 'R');
+
+//         // Déplacement à la ligne suivante
 //         $this->Ln();
 //     }
-//     // Closing line
-//     $this->Cell(array_sum($w),0,'','T');
+
+//     // Ligne de clôture
+//     $this->Cell(array_sum($w), 0, '', 'T');
 // }
-function Table($header, $data) {
-    // Largeurs des colonnes
-    $w = [50, 50, 40, 30, 30]; // Largeur pour chaque colonne : Titre, Abonnement, Catégorie, Jours, Prix
-    $this->SetFont('Arial', 'B', 12); // Police pour les en-têtes
-
-    // Afficher les en-têtes
-    foreach ($header as $i => $col) {
-        $this->Cell($w[$i], 7, utf8_decode($col), 1, 0, 'C'); // En-têtes centrées
-    }
-    $this->Ln();
-
-    // Données
-    $this->SetFont('Arial', '', 12); // Police normale pour les données
-    foreach ($data as $row) {
-        // Colonne 1 : Titre avec MultiCell pour gérer les noms longs
-        $x = $this->GetX(); // Position X courante
-        $y = $this->GetY(); // Position Y courante
-        $this->MultiCell($w[0], 6, utf8_decode($row[0]), 'LR'); // MultiCell gère les retours à la ligne
-        $this->SetXY($x + $w[0], $y); // Ajuste la position pour les colonnes suivantes
-
-        // Colonne 2 : Type d'abonnement
-        $this->Cell($w[1], 6, utf8_decode($row[1]), 1, 0, 'C');
-
-        // Colonne 3 : Catégorie
-        $this->Cell($w[2], 6, utf8_decode($row[2]), 1, 0, 'C');
-
-        // Colonne 4 : Jours
-        $this->Cell($w[3], 6, $row[3], 1, 0, 'C');
-
-        // Colonne 5 : Prix TTC
-        $this->Cell($w[4], 6, number_format($row[4], 2, ',', ' ') . ' €', 1, 0, 'R');
-
-        // Déplacement à la ligne suivante
-        $this->Ln();
-    }
-
-    // Ligne de clôture
-    $this->Cell(array_sum($w), 0, '', 'T');
-}
 
 }
 
