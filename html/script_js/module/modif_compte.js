e3d25eb2bf799c422af0ae588e0d24c9d2c7bf27
5cover <thediscover22450@gmail.com>
2025-01-15 12:29:46 +0100

    pg

--- html/script_js/module/modif_compte.js
+++ html/script_js/module/modif_compte.js
@@ -1,7 +1,7 @@
 
 const input_api_key = document.getElementById('api_key');
-const button_regenerate_api_key = document.getElementById('button-regenerate-api_key');
-const button_delete_api_key = document.getElementById('button-delete-api_key');
+const button_regenerate_api_key = document.getElementById('button-regenerate-api-key');
+const button_delete_api_key = document.getElementById('button-delete-api-key');
 
 button_regenerate_api_key.addEventListener('click', () => input_api_key.value = crypto.randomUUID());
 button_delete_api_key.addEventListener('click', () => input_api_key.value = '');
