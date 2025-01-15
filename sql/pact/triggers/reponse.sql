930757658c11c7e45b07991ca879cb4f457f3e4c
5cover <thediscover22450@gmail.com>
2025-01-15 09:23:07 +0100

    forgot begin and end

--- sql/pact/triggers/reponse.sql
+++ sql/pact/triggers/reponse.sql
@@ -5,12 +5,14 @@ set
 
 -- Create
 create function reponse_insert () returns trigger as $$
+begin
     insert into pact._signalable default values returning id into new.id;
     insert into
         pact._reponse (id, id_avis, contenu)
     values
         (new.id, new.id_avis, new.contenu);
     return new;
+end
 $$ language plpgsql;
 
 create trigger tg_reponse_insert instead of insert on reponse for each row
