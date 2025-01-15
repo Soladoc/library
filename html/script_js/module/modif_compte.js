39a828b4a7b2bc30f2e14a4b9320ed42bb3f91cb
5cover <thediscover22450@gmail.com>
2025-01-15 12:28:30 +0100

    really? yeah, really.

--- html/script_js/module/modif_compte.js
+++ html/script_js/module/modif_compte.js
@@ -1,7 +1,7 @@
 
 const input_api_key = document.getElementById('api_key');
-const button_regenerate_api_key = document.getElementById('button-regenerate-uuid');
-const button_delete_api_key = document.getElementById('button-delete-uuid');
+const button_regenerate_api_key = document.getElementById('button-regenerate-api_key');
+const button_delete_api_key = document.getElementById('button-delete-api_key');
 
 button_regenerate_api_key.addEventListener('click', () => input_api_key.value = crypto.randomUUID());
 button_delete_api_key.addEventListener('click', () => input_api_key.value = '');
