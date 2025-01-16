2b56258edec0266e042bb444dc92f60f143be5ae
Scover <bardini.raphael@gmail.com>
2025-01-16 15:06:53 +0100

    fix syntax error

--- sql/pact/triggers/avis.sql
+++ sql/pact/triggers/avis.sql
@@ -22,7 +22,7 @@ execute function avis_insert ();
 create function avis_update () returns trigger as $$
 begin
     if old.id_offre <> new.id_offre then
-        raise 'Ne peut pas modifier id_offre'
+        raise 'Ne peut pas modifier id_offre';
     end if;
     new = update_avis(old, new);
     return new;
