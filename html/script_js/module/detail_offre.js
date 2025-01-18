a5d2e4af04c2af7cde5e5b4b46455e70b29a902c
Scover <bardini.raphael@gmail.com>
2025-01-18 12:49:08 +0100

    fix js error

--- html/script_js/module/detail_offre.js
+++ html/script_js/module/detail_offre.js
@@ -1,6 +1,6 @@
 import { scroller } from './util.js';
 
 const review_form = document.getElementById('review-form');
-if (review_form.getElementsByClassName('message').item(0).childElementCount > 0) {
+if (review_form?.getElementsByClassName('message').item(0).childElementCount > 0) {
     scroller(review_form);
 }
