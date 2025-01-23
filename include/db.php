b0e6b9bd03863770cf278824a61889277d7f663f
Scover <bardini.raphael@gmail.com>
2025-01-23 19:28:23 +0100

    inverted conditional

--- include/db.php
+++ include/db.php
@@ -22,7 +22,7 @@ function connect(): PDO
 
     // Load .env file
     {
-        $envfile = __DIR__ . (is_localhost() ? '/.env' : '/raphael.env');
+        $envfile = __DIR__ . (is_localhost() ? '/raphael.env' : '/.env');
         foreach (notfalse(file($envfile, FILE_SKIP_EMPTY_LINES), "dotenv file missing at $envfile") as $line) {
             notfalse(putenv(trim($line)));
         }
