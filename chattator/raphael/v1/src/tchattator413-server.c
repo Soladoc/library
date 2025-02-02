0f594a5bc10be16d85f6098b89b9ea7eaf43c2d7
Scover <bardini.raphael@gmail.com>
2025-02-02 19:43:59 +0100

    server

--- chattator/raphael/v1/src/tchattator413-server.c
+++ chattator/raphael/v1/src/tchattator413-server.c
@@ -14,7 +14,17 @@
 #define SERVER_ADDR "127.0.0.1"
 
 static inline void interpret_request(cfg_t *cfg, db_t *db, server_t *server, int fd) {
-    json_object *obj_input = json_object_from_fd(fd);
+    put_log("interpreting request from fd %d\n", fd);
+
+    char buf[BUFSIZ] = { 0 };
+    ssize_t bytes_read = read(fd, buf, sizeof buf - 1);
+    if (bytes_read > 0) buf[bytes_read] = '\0';
+
+    json_object *obj_input = json_tokener_parse(buf);
+    // if !obj_input : invalid JSON recieved
+
+    put_log("received json input, interpreting request\n");
+
     json_object *obj_output = tchattator413_interpret(obj_input, cfg, db, server, NULL, NULL, NULL);
     json_object_put(obj_input);
 
@@ -22,15 +32,18 @@ static inline void interpret_request(cfg_t *cfg, db_t *db, server_t *server, int
     ssize_t bytes_written;
     char const *output = json_object_to_json_string_length(obj_output, JSON_C_TO_STRING_PLAIN, &len);
     ++len; // include null terminator
+    put_log("preparing to write %zu bytes of response\n", len);
 
     do {
         bytes_written = write(fd, output, len);
         if (-1 == bytes_written) errno_exit("write");
         len -= bytes_written;
         output += bytes_written;
+        put_log("wrote %zd bytes, %zu remaining\n", bytes_written, len);
     } while (len > 0);
 
     json_object_put(obj_output);
+    put_log("request interpretation completed for fd %d\n", fd);
 }
 
 static int gs_sock = -1;
@@ -44,15 +57,18 @@ static void close_sock(int sig) {
 }
 
 int tchattator413_run_server(cfg_t *cfg, db_t *db, server_t *server) {
+    put_log("initializing server...\n");
     // Acquérir le socket
     gs_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (-1 == gs_sock) errno_exit("socket");
     int sock_opt = 1;
     setsockopt(gs_sock, SOL_SOCKET, SO_REUSEADDR, &sock_opt, sizeof sock_opt);
+    put_log("socket created with fd %d\n", gs_sock);
 
     // Programmer sa libération sur Ctrl+C
     if (SIG_ERR == signal(SIGINT, close_sock)) errno_exit("signal");
     if (SIG_ERR == signal(SIGTERM, close_sock)) errno_exit("signal");
+    put_log("signal handlers installed\n");
 
     struct sockaddr_in server_addr = {
         .sin_addr.s_addr = inet_addr(SERVER_ADDR),
@@ -64,11 +80,13 @@ int tchattator413_run_server(cfg_t *cfg, db_t *db, server_t *server) {
         perror("bind");
         return EXIT_FAILURE;
     }
+    put_log("socket bound to address " SERVER_ADDR ":%hu\n", cfg_port(cfg));
 
     if (-1 == listen(gs_sock, cfg_backlog(cfg))) {
         perror("listen");
         return EXIT_FAILURE;
     }
+    put_log("listening with backlog of %d\n", cfg_backlog(cfg));
 
     put_log("server started on " SERVER_ADDR " port %hu\n", cfg_port(cfg));
 
@@ -76,19 +94,26 @@ int tchattator413_run_server(cfg_t *cfg, db_t *db, server_t *server) {
     int size = sizeof addr_connection;
 
     while (true) {
+        put_log("waiting for new connection...\n");
         int fd = accept(gs_sock, (struct sockaddr *)&addr_connection, (socklen_t *)&size);
         if (-1 == fd) {
             // If a signal interrupted accept().
             if (EINTR == errno) {
                 // If the signal handler decided to exit
                 if (gs_sock == -1) break;
+                put_log("accept interrupted by signal, continuing...\n");
                 continue;
             }
             errno_exit("accept");
         }
+        put_log("accepted new connection from %s:%d with fd %d\n",
+            inet_ntoa(addr_connection.sin_addr),
+            ntohs(addr_connection.sin_port),
+            fd);
 
         interpret_request(cfg, db, server, fd);
 
+        put_log("closing connection fd %d\n", fd);
         close(fd);
     }
 
