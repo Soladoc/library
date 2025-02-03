26501c996c732ad337546b07fecb03e0227bb96d
Scover <bardini.raphael@gmail.com>
2025-02-03 10:01:09 +0100

    fix

--- script/server/reset-database.bash
+++ script/server/reset-database.bash
@@ -10,6 +10,8 @@ cd /docker/sae/data
 sudo git fetch --all
 sudo git reset --hard origin/main
 
+# the "sae" database is now an empty placeholder for connection
+
 for instance in main test; do
     db="sae413_$instance"
     bash sql/unite.bash $instance | sudo docker exec -iw / postgresdb psql -v ON_ERROR_STOP=on -h localhost -wU "$DB_USER" \
