34c90fc96fcb4538022419f7e2bcdef3ecb17d57
Scover <bardini.raphael@gmail.com>
2025-02-16 10:41:29 +0100

    let's try this

--- script/server/restart.bash
+++ script/server/restart.bash
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+# $1: env files contents
+
 set -eu
 
 cd /docker/sae
-echo "$DOTENV" > .env
+echo "$1" > .env
 set -a
 #shellcheck source=/dev/null
-source .env
+. .env
 set +a
 
 docker-compose -f data/docker-compose.yml down
