dbd96f17ef1e4c34cd63be3fc67e0c9dce03aeea
Scover <bardini.raphael@gmail.com>
2025-02-16 11:45:43 +0100

    default db postgres

--- script/server/reset-database.bash
+++ script/server/reset-database.bash
@@ -11,11 +11,9 @@ sudo git reset --hard origin/main
 sudo git submodule init
 sudo git submodule update --remote --merge
 
-# the "sae" database is now an empty placeholder for connection
-
 for instance in main test; do
     db="sae413_$instance"
-    bash sql/unite.bash $instance | sudo docker exec -iw / postgresdb psql -v ON_ERROR_STOP=on -h localhost -wU "$DB_USER" \
+    bash sql/unite.bash $instance | sudo docker exec -iw / postgresdb psql -d postgres -v ON_ERROR_STOP=on -h localhost -wU "$DB_USER" \
         -c "drop database if exists $db" \
         -c "create database $db" \
         -c "\c $db" \
