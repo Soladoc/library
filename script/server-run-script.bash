e07ed6ba84f6fa4b36a7bc1b1a6aa11b3c7a8957
Scover <bardini.raphael@gmail.com>
2025-02-16 10:43:08 +0100

    fix?

--- script/server-run-script.bash
+++ script/server-run-script.bash
@@ -8,4 +8,7 @@ mkdir -p ~/.ssh
 echo "$ARTIFACT_SSH_KEY" >~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -p 22 "$ARTIFACT_HOST" >>~/.ssh/known_hosts
-ssh "debian@$ARTIFACT_HOST" sudo bash <"$1"
+readonly script="$1"
+shift
+# shellcheck disable=SC2029
+ssh "debian@$ARTIFACT_HOST" sudo bash "$@" <"$script"
