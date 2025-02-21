6894eb686dd3b081d0bcf81419bd301570ff5962
Benjamin <benjamin.dumontgirard@gmail.com>
2025-02-21 11:41:18 +0100

    Push auto de Benjamin

---
+++ html/secret/la_maison-de-Benoit.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La maison de benoit</title>
+</head>
+<body>
+    <section class="Preanbule">
+
+        <p>
+            Bonjour Benoit, j'ai vu ta détresse et je suis déterminé a te donner le repos que tu mérite!
+            Ici rien n'est atroce, c'est juste une page html sans php, sans js et sans css. En bref c'est un havre de paix 
+            simple, qui te détendra.
+            je vais ajouter quelques petites activitées pour toi afin que tu puisse te détendre en paix!
+        </p>
+    </section>
+    <section class="Le jardin">
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fjacuzzi.fr%2Fspas-jacuzzi%2Fj-lx%2Fspa-6-places-j-lx%2F&psig=AOvVaw3O30Al159hy7AE21SUniOx&ust=1740218113681000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjzo-O_1IsDFQAAAAAdAAAAABAE" alt="Jaccuzi de Benoit">
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Faguaconstruction.com%2Ftop-palm-coast-luxury-pool-features-unveiled%2F&psig=AOvVaw3iatB_0PhlOL0dDlxaq41n&ust=1740220267462000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjKhObH1IsDFQAAAAAdAAAAABAE" alt="La piscine">
+    </section>
+    <section class="Le salon">
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.carpentry.sg%2F2023%2F03%2F16%2F5-ultra-luxury-living-room-interior%2F&psig=AOvVaw1fhnIKCujBkGhNVzAup35i&ust=1740220312747000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKDb0fzH1IsDFQAAAAAdAAAAABAE" alt="le salon">
+    </section>
+    <section class="La chambre">
+        <div class="Le lit">
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.heavenlybeds.co.uk%2Fproducts%2Fambassador-sleigh-bed&psig=AOvVaw1aWmFRbTls4mX9We0-nG6I&ust=1740220361878000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJiR4pbI1IsDFQAAAAAdAAAAABAE" alt="Le lit">
+            <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiRyZK1yNSLAxUFV6QEHcZ-OFcQFnoECFoQAQ&url=https%3A%2F%2Ffr.wikipedia.org%2Fwiki%2FMargot_Robbie&usg=AOvVaw3NHxCTtXSdVYDOUy_hBNB3&opi=89978449">La femme de Benoit</a>
+        </div>
+    </section>
+    <section class="Le garage">
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.youtube.com%2Fwatch%3Fv%3D2GRenSvGBNA%26pp%3DygUII2ZsYWV4dXM%253D&psig=AOvVaw1bW6F73h3t8kt7BmEA-DGo&ust=1740220534743000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMj2xebI1IsDFQAAAAAdAAAAABAE" alt="Le garage de benoit">
+    </section>
+</body>
+</html>
