a42f5ce49cf9a19c83159cfc7496040ffe1a5276
Soladoc <romaingrandchamppro@gmail.com>
2025-10-17 09:21:30 +0200

    supprime chattator

--- chattator/password_hash.php
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env php
-<?= password_hash(rtrim(stream_get_contents(STDIN), "\n"), PASSWORD_DEFAULT);
--- chattator/tct413/test/stb_test_impl.c
+++
@@ -1,7 +0,0 @@
-/// @file
-/// @author Raphaël
-/// @brief stb_test.h implementation
-/// @date 1/02/2025
-
-#define STB_TEST_IMPLEMENTATION
-#include <stb_test.h>
