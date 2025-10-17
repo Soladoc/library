fa62583c9cc0dbeb2bca393b640e152fd0a31ff8
Soladoc <romaingrandchamppro@gmail.com>
2025-10-17 09:20:51 +0200

    supprime offres

--- sql/instances/test/tchatator/data.sql
+++
@@ -1,9 +0,0 @@
-set schema 'tchatator';
-
--- Pour l'instant, la suite de teste Tchatator attend une BDD vide.
-
-/*insert into
-    _msg (content, id_compte_sender, id_compte_recipient)
-values
-    ('Bonjour pro_1 (1er message)', 5, 1),
-    ('Bonjour pro_2 (2eme message)', 5, 2);*/
