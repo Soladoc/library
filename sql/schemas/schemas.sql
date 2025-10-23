d199245dd6ef917ce9dba9f265fdc5447df2df3f
Soladoc <romaingrandchamppro@gmail.com>
2025-10-23 16:50:09 +0200

    schema

--- sql/schemas/schemas.sql
+++ sql/schemas/schemas.sql
@@ -2,8 +2,5 @@ select pg_terminate_backend(pid)
     from pg_stat_activity
     where pid <> pg_backend_pid();
 
-drop schema if exists pact cascade;
-create schema pact;
-
-drop schema if exists tchatator cascade;
-create schema tchatator;
+drop schema if exists library cascade;
+create schema library;
